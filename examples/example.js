1b310eb78c71a4f48ac86d4d009d6993878b8b95
greta.mcgivern <greta.matulionyte@panintelligence.com>
2025-02-04 10:53:36 +0000

    example with dc implemented

--- examples/example.js
+++
@@ -1,6 +0,0 @@
-import PiEmbedActions from '../src/main.js';
-
-const dashboard2 = PiEmbedActions.createDashboard('dashboard2', 'https://pi-dev.uk:8224/pi?lang=en_GB&editorDisplayMode=CONTENT');
-dashboard2.reportEditor.open(123);
-dashboard2.reportEditor.open(200);
-dashboard2.dataSourceItemEditor.open(2);
