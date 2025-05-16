d94b27e354f8f0066aea41f7dae169113f3d101c
greta.mcgivern <greta.mcgivern@panintelligence.com>
2025-05-16 13:14:03 +0100

    #2455 add cookieName query param to docs

--- src/pi-embed-actions.js
+++ src/pi-embed-actions.js
@@ -32,7 +32,7 @@ export class PiEmbedActions {
      * @param {string} iframeId - The unique identifier for the iframe in which the dashboard is embedded.
      * @param {string} dashboardUrl - The URL for the dashboard.
      * ```text
-     *    https://pi-dev.uk:8224/pi?lang=en_GB&editorDisplayMode=CONTENT
+     *    https://pi-dev.uk:8224/pi?lang=en_GB&editorDisplayMode=CONTENT&cookieName=Test
      *    |____| |_______|  |__|   |__________________________________|
      *      |        |       |                       |
      *   Protocol  Host    Port    List of query parameters in key-value format,
@@ -44,6 +44,7 @@ export class PiEmbedActions {
      * - **`locale`**: Dashboard language and regional settings (e.g., `lang=en_GB`).
      * - **`token`**: Secure access token for authentication (e.g., `token=xxx`).
      * - **`editorDisplayMode`**: Editor display mode (e.g., `editorDisplayMode=CONTENT`). See {@link EditorDisplayMode} for more details.
+     * - **`cookieName`**: Dynamic cookie name used to specify the name of the cookie that stores the token (e.g., `cookieName=Test`).
      *
      * @returns {PiEmbedActions}
      *
